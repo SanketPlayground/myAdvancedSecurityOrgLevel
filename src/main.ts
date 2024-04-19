6205a5609808f765b53350f0db0526b6110d074f
Sanket Desai <sanketde@canarys.com>
2024-04-19 15:40:03 +0530

    firt commit

--- src/main.ts
+++ src/main.ts
@@ -1,107 +1,76 @@
-import * as core from '@actions/core'
-import * as xlsx from 'xlsx'
-import {Octokit} from '@octokit/rest' //import to call rest api
-import Pivot from 'quick-pivot'
-import {graphql} from '@octokit/graphql' //import to call graphql api
+import * as core from '@actions/core';
+import * as xlsx from 'xlsx';
+import { Octokit } from '@octokit/rest';
+import Pivot from 'quick-pivot';
+import { graphql } from '@octokit/graphql';
 
 async function run(): Promise<void> {
   try {
-    const token = core.getInput('token')
+    const token = core.getInput('token');
     if (!token) {
-      core.error('please set the INPUT_TOKEN env variable')
-    }
-    //fetch data using octokit api
-    const octokit = new Octokit({
-      auth: token //get the token from the action inputs.
-    })
-
-    let repo: string = core.getInput('repo')
-    let login = ''
-    let repoName = ''
-    if (!repo) {
-      //required to run locally
-      repo = process.env.GITHUB_REPOSITORY!
+      core.error('Please set the INPUT_TOKEN env variable');
+      return;
     }
 
-    if (repo) {
-      login = repo.split('/')[0]
-      repoName = repo.split('/')[1]
-    } else {
-      core.error(
-        'Could not find repo, please set the GITHUB_REPOSITORY env variable'
-      )
+    const octokit = new Octokit({ auth: token });
+
+    const org = core.getInput('org');
+    if (!org) {
+      core.error('Please provide the organization name');
+      return;
     }
 
-    //get dependency graph vulnerability report
-    const dgIssues: string[][] = await getDependabotReport(login, repoName)
+    const repos = await getOrganizationRepos(octokit, org);
+    if (!repos || repos.length === 0) {
+      core.error(`No repositories found in organization ${org}`);
+      return;
+    }
 
-    //get the code scanning report for repo.
-    const csIssues: string[][] = await getCodeScanningReport(
-      login,
-      repoName,
-      octokit
-    )
+    const allData: {
+      [key: string]: {
+        dgIssues: string[][];
+        csIssues: string[][];
+        dgInfo: string[][];
+        secretScanningAlerts: string[][];
+      };
+    } = {};
 
-    //get the dependency graph report for repo.
-    const dgInfo: string[][] = await getDependencyGraphReport(login, repoName)
+    for (const repo of repos) {
+      const [login, repoName] = repo.split('/');
+      const dgIssues = await getDependabotReport(login, repoName, token);
+      const csIssues = await getCodeScanningReport(login, repoName, octokit);
+      const dgInfo = await getDependencyGraphReport(login, repoName, token);
+      const secretScanningAlerts = await getSecretScanningReport(octokit, login, repoName);
 
-    //fetch secret scanning alerts from github repo
-    const secretScanningAlerts: string[][] = await getSecretScanningReport(
-      octokit,
-      login,
-      repoName
-    )
+      allData[repo] = {
+        dgIssues,
+        csIssues,
+        dgInfo,
+        secretScanningAlerts
+      };
+    }
 
-    // const Pivot = require('quick-pivot')
-
-    const dgPivotData: string[][] = generatePivot(
-      ['manifest'],
-      ['licenseInfo'],
-      'packageName',
-      'count',
-      dgInfo
-    )
-
-    const csPivotData: string[][] = generatePivot(
-      ['cwe'],
-      ['severity'],
-      'html_url',
-      'count',
-      csIssues
-    )
-
-    //console.log(pivotData)
-
-    //create an excel file with the dataset
-    const wb = xlsx.utils.book_new()
-    const ws = xlsx.utils.aoa_to_sheet(csIssues)
-    const ws1 = xlsx.utils.aoa_to_sheet(dgInfo)
-    const ws2 = xlsx.utils.aoa_to_sheet(dgPivotData)
-    const ws3 = xlsx.utils.aoa_to_sheet(csPivotData)
-    const ws4 = xlsx.utils.aoa_to_sheet(secretScanningAlerts)
-    const ws5 = xlsx.utils.aoa_to_sheet(dgIssues)
-
-    xlsx.utils.book_append_sheet(wb, ws, 'code-scanning-issues')
-    xlsx.utils.book_append_sheet(wb, ws1, 'dependencies-list')
-    xlsx.utils.book_append_sheet(wb, ws2, 'dependencies-license-pivot')
-    xlsx.utils.book_append_sheet(wb, ws3, 'code-scanning-Pivot')
-    xlsx.utils.book_append_sheet(wb, ws4, 'secret-scanning-alerts')
-    xlsx.utils.book_append_sheet(wb, ws5, 'software-composition-analysis')
-
-    xlsx.writeFile(wb, 'alerts.xlsx')
+    createExcel(allData);
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-run()
+run();
+
+async function getOrganizationRepos(octokit: Octokit, org: string): Promise<string[]> {
+  const response = await octokit.paginate(octokit.rest.repos.listForOrg, {
+    org,
+  });
+  return response.map((repo: any) => repo.full_name);
+}
 
 async function getSecretScanningReport(
   octokit: Octokit,
   login: string,
   repoName: string
 ): Promise<string[][]> {
-  const csvData: string[][] = []
+  const csvData: string[][] = [];
 
   try {
     const secretScanningAlerts = await octokit.paginate(
@@ -110,7 +79,7 @@ async function getSecretScanningReport(
         owner: login,
         repo: repoName
       }
-    )
+    );
 
     const header: string[] = [
       'html_url',
@@ -118,9 +87,9 @@ async function getSecretScanningReport(
       'secret',
       'state',
       'resolution'
-    ]
+    ];
 
-    csvData.push(header)
+    csvData.push(header);
     for (const alert of secretScanningAlerts) {
       const row: string[] = [
         alert.html_url!,
@@ -128,48 +97,17 @@ async function getSecretScanningReport(
         alert.secret!,
         alert.state!,
         alert.resolution!
-      ]
-      csvData.push(row)
+      ];
+      csvData.push(row);
     }
-    return csvData
+    return csvData;
   } catch (error) {
     if (error instanceof Error) {
-      core.error(error.message)
-      csvData.push([error.message, '', '', '', ''])
+      core.error(error.message);
+      csvData.push([error.message, '', '', '', '']);
     }
-    return csvData
+    return csvData;
   }
-}
-
-function generatePivot(
-  rowHeader: string[],
-  colHeader: string[],
-  aggregationHeader: string,
-  aggregator: string,
-  dgInfo: string[][]
-): string[][] {
-  //const columnsToPivot = [`${colHeader}`]
-  //const rowsToPivot = [`${rowHeader}`]
-  const aggregationDimensions = [`${aggregationHeader}`]
-  //const aggregator = 'count'
-  const pivot = new Pivot(
-    dgInfo,
-    rowHeader,
-    colHeader,
-    aggregationDimensions,
-    aggregator
-  )
-  //console.log(pivot.data.table)
-  const pivotData: string[][] = []
-  for (const row of pivot.data.table) {
-    const pivotRow: string[] = []
-    for (const col of row.value) {
-      pivotRow.push(col)
-      //console.log(col)
-    }
-    pivotData.push(pivotRow)
-  }
-  return pivotData
 }
 
 async function getCodeScanningReport(
@@ -184,10 +122,10 @@ async function getCodeScanningReport(
       owner: login,
       repo: repoName
     }
-  )
+  );
 
   // create a array of objects with the data
-  const csvData: string[][] = []
+  const csvData: string[][] = [];
   const header: string[] = [
     'toolName',
     'toolVersion',
@@ -205,27 +143,27 @@ async function getCodeScanningReport(
     'fixedAt',
     'dismissedAt',
     'dismissedBy'
-  ]
+  ];
 
-  csvData.push(header)
+  csvData.push(header);
   //iterate over the data and print the alert information
   for (const alert of data) {
     //create an array of string values
-    const rule: any = alert.rule
-    let securitySeverity = ''
-    let securityCwe = ''
+    const rule: any = alert.rule;
+    let securitySeverity = '';
+    let securityCwe = '';
     if (rule.security_severity_level) {
-      securitySeverity = rule.security_severity_level
+      securitySeverity = rule.security_severity_level;
     } else {
-      securitySeverity = rule.severity
+      securitySeverity = rule.severity;
     }
     for (const cwe of rule.tags) {
       if (cwe.includes('external/cwe/cwe')) {
-        securityCwe = `${securityCwe}${cwe}, `
+        securityCwe = `${securityCwe}${cwe}, `;
       }
     }
-    securityCwe = securityCwe.replace(/,\s*$/, '')
-    const _alert: any = alert
+    securityCwe = securityCwe.replace(/,\s*$/, '');
+    const _alert: any = alert;
     const row: string[] = [
       alert.tool.name!,
       alert.tool.version!,
@@ -243,19 +181,21 @@ async function getCodeScanningReport(
       _alert.fixed_at,
       alert.dismissed_at,
       alert.dismissed_by
-    ]
+    ];
 
-    csvData.push(row)
+    csvData.push(row);
   }
 
-  return csvData
+  return csvData;
 }
+
 async function getDependencyGraphReport(
   login: string,
-  repoName: string
+  repoName: string,
+  token: string
 ): Promise<string[][]> {
   //get the dependency graph for the repo and parse the data
-  const {repository} = await graphql(
+  const { repository } = await graphql(
     `
       {
         repository(owner: "${login}", name: "${repoName}") {
@@ -290,32 +230,32 @@ async function getDependencyGraphReport(
     `,
     {
       headers: {
-        authorization: `token ${core.getInput('token')}`,
+        authorization: `token ${token}`,
         accept: 'application/vnd.github.hawkgirl-preview+json'
       }
     }
-  )
+  );
 
-  const csvData: string[][] = []
+  const csvData: string[][] = [];
   const header: string[] = [
     'manifest',
     'packageName',
     'packageManager',
     'requirements',
     'licenseInfo'
-  ]
+  ];
 
-  csvData.push(header)
+  csvData.push(header);
   for (const dependency of repository.dependencyGraphManifests.edges) {
     for (const dependencyEdge of dependency.node.dependencies.edges) {
-      let licenseInfo = ''
+      let licenseInfo = '';
       if (
         //null checks in case a dependency has no license info
         dependencyEdge.node &&
         dependencyEdge.node.repository &&
         dependencyEdge.node.repository.licenseInfo
       ) {
-        licenseInfo = dependencyEdge.node.repository.licenseInfo.name
+        licenseInfo = dependencyEdge.node.repository.licenseInfo.name;
       }
       const row: string[] = [
         dependency.node.filename,
@@ -323,19 +263,20 @@ async function getDependencyGraphReport(
         dependencyEdge.node.packageManager,
         dependencyEdge.node.requirements,
         licenseInfo
-      ]
+      ];
 
-      csvData.push(row)
+      csvData.push(row);
     }
   }
-  return csvData
+  return csvData;
 }
 
 async function getDependabotReport(
   login: string,
-  repoName: string
+  repoName: string,
+  token: string
 ): Promise<string[][]> {
-  const csvData: string[][] = []
+  const csvData: string[][] = [];
   const header: string[] = [
     'ghsaId',
     'packageName',
@@ -343,22 +284,22 @@ async function getDependabotReport(
     'severity',
     'firstPatchedVersion',
     'description'
-  ]
+  ];
 
-  csvData.push(header)
+  csvData.push(header);
 
   try {
     //get the dependency graph for the repo and parse the data
-    let response
-    let after = ''
+    let response;
+    let after = '';
     do {
-      response = await fetchAPIResults(login, repoName, after)
-      after = response.repository.vulnerabilityAlerts.pageInfo.endCursor
+      response = await fetchAPIResults(login, repoName, after, token);
+      after = response.repository.vulnerabilityAlerts.pageInfo.endCursor;
       for (const dependency of response.repository.vulnerabilityAlerts.nodes) {
-        let version = 'na'
+        let version = 'na';
         if (dependency.securityVulnerability.firstPatchedVersion != null)
           version =
-            dependency.securityVulnerability.firstPatchedVersion.identifier
+            dependency.securityVulnerability.firstPatchedVersion.identifier;
 
         const row: string[] = [
           dependency.securityVulnerability.advisory.ghsaId,
@@ -367,33 +308,35 @@ async function getDependabotReport(
           dependency.securityVulnerability.advisory.severity,
           version,
           dependency.securityVulnerability.advisory.description
-        ]
+        ];
 
-        csvData.push(row)
+        csvData.push(row);
       }
-    } while (response.repository.vulnerabilityAlerts.pageInfo.hasNextPage)
+    } while (response.repository.vulnerabilityAlerts.pageInfo.hasNextPage);
 
-    return csvData
+    return csvData;
   } catch (error) {
     if (error instanceof Error) {
-      core.error(error.message)
-      csvData.push([error.message, '', '', '', ''])
+      core.error(error.message);
+      csvData.push([error.message, '', '', '', '']);
     }
-    return csvData
+    return csvData;
   }
 }
+
 async function fetchAPIResults(
   login: string,
   repoName: string,
-  after: string
+  after: string,
+  token: string
 ): Promise<any> {
   const response: any = await graphql(getQuery(login, repoName, after), {
     headers: {
-      authorization: `token ${core.getInput('token')}`,
+      authorization: `token ${token}`,
       accept: 'application/vnd.github.hawkgirl-preview+json'
     }
-  })
-  return response
+  });
+  return response;
 }
 
 function getQuery(login: string, repoName: string, after: string): string {
@@ -428,6 +371,32 @@ function getQuery(login: string, repoName: string, after: string): string {
           }
         }
       }
-    `
-  return query
+    `;
+  return query;
+}
+
+function createExcel(allData: {
+  [key: string]: {
+    dgIssues: string[][];
+    csIssues: string[][];
+    dgInfo: string[][];
+    secretScanningAlerts: string[][];
+  };
+}) {
+  const wb = xlsx.utils.book_new();
+
+  for (const repo in allData) {
+    const { dgIssues, csIssues, dgInfo, secretScanningAlerts } = allData[repo];
+    const ws1 = xlsx.utils.aoa_to_sheet(dgIssues);
+    const ws2 = xlsx.utils.aoa_to_sheet(csIssues);
+    const ws3 = xlsx.utils.aoa_to_sheet(dgInfo);
+    const ws4 = xlsx.utils.aoa_to_sheet(secretScanningAlerts);
+
+    xlsx.utils.book_append_sheet(wb, ws1, `${repo}_dependabot_report`);
+    xlsx.utils.book_append_sheet(wb, ws2, `${repo}_code_scanning_report`);
+    xlsx.utils.book_append_sheet(wb, ws3, `${repo}_dependency_graph_report`);
+    xlsx.utils.book_append_sheet(wb, ws4, `${repo}_secret_scanning_report`);
+  }
+
+  xlsx.writeFile(wb, 'all_reports.xlsx');
 }
